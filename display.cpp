18f647877f4ae7c70cccb8aefe7e69b113cf0120
Dirk Heijnen <dirkheijnen95@gmail.com>
2024-05-18 13:48:30 +0200

    Added basic help screen functionality

---
+++ display.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void showHelp(){
+	 std::cout
+    << "\n"
+    << "Koala Usage: \n"
+    << " command       args                               description\n"
+    << " -------       ----                               -----------\n"
+    << " -h, --help    [none]		                  | Shows this help display \n\n";
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
+#include <iostream>
 #include "helpers.cpp"
+#include "display.cpp"
 
 int main(int argc, char *argv[]) {
+	int exitCode = -1;
+
 	
-	int inputNumber = waitForUserInput();
-	printMessage(inputNumber);
-	
-	return 0;
+	if (argc == 1) {	
+		showHelp();
+		exitCode = 0;
+	}
+
+	for (int i = 0; i < argc; ++i){
+		std::cout << "Arg " << i << ": " << argv[i] << std::endl;
+		exitCode = 0;
+	}
+
+	return exitCode;
 }
